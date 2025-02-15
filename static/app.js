a06afc7cfd058eb7b6f2565238fc1f5585d8f215
Darren Holzhausen <darrenholzhausen@gmail.com>
2025-02-15 13:34:43 -0600

    new file

--- static/app.js
+++ static/app.js
@@ -3,20 +3,24 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-
+    const metadata = data.metadata;  
 
     // Filter the metadata for the object with the desired sample number
+    const sampleMetadata = metadata.filter(sampleObj => sampleObj.id === sample)[0];
 
 
     // Use d3 to select the panel with id of `#sample-metadata`
+    const metadataPanel = d3.select("#sample-metadata");
 
 
     // Use `.html("") to clear any existing metadata
-
+    metadataPanel.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-
+    Object.entries(sampleMetadata).forEach(([KeyboardEvent, value]) => {
+      metadataPanel.append("h6").text(`${key}: ${value}`);  
+    })
   });
 }
 
@@ -25,10 +29,10 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-
+    const samples_field = data.samples
 
     // Filter the samples for the object with the desired sample number
-
+    const sampleSamples = d3.select()
 
     // Get the otu_ids, otu_labels, and sample_values
 
